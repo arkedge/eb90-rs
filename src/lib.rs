a625405ac4c931d98440ae366aad8159ac945995
Hidekazu Kobayashi <koba789@arkedgespace.com>
2023-06-02 17:10:45 +0000

    Fix CRC algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ pub use parser::Parser;
 #[cfg(all(feature = "codec"))]
 pub mod codec;
 #[cfg(feature = "codec")]
-pub use codec::{Encoder, Decoder};
+pub use codec::{Decoder, Encoder};
 
 pub const STX: [u8; 2] = [0xeb, 0x90];
 pub const ETX: [u8; 2] = [0xc5, 0x79];
