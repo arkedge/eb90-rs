e2c7762e3cde8feecd988a9f3cd798438dc963bd
Hidekazu Kobayashi <koba789@arkedgespace.com>
2023-06-02 17:10:45 +0000

    Fix CRC algorithm

--- src/crc.rs
+++ src/crc.rs
@@ -1,4 +1,4 @@
-pub const ALGO: crc::Crc<u16> = crc::Crc::<u16>::new(&crc::CRC_16_ARC);
+pub const ALGO: crc::Crc<u16> = crc::Crc::<u16>::new(&crc::CRC_16_IBM_3740);
 
 #[cfg(test)]
 mod tests {
@@ -9,6 +9,6 @@ mod tests {
     fn test_crc() {
         let input = DEADBEEF;
         let crc = ALGO.checksum(input).to_be_bytes();
-        assert_eq!(crc, [0xe5, 0x9b]);
+        assert_eq!(crc, [0x40, 0x97]);
     }
 }
